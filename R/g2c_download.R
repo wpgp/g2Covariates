0a660c8abe5caa70829e23806c5e6eeb500014d0
Maksym Bondarenko <m.bondarenko@gmail.com>
2025-05-07 17:26:52 +0100

    Update g2c_download.R

--- R/g2c_download.R
+++ R/g2c_download.R
@@ -40,16 +40,6 @@ g2c_download <- function(covariate=NULL,
 
   '%!in%' <- function(x,y)!('%in%'(x,y))
 
-  # covariate <- "built_S_dist"
-  # ISO <- "MLI"
-  # prj_year=2015
-  # output_dir <- "E:\\WorldPop\\R_Script\\Covariates_Downloading\\output"
-  # ftp_srv=FALSE
-  # method="auto"
-  # quiet=FALSE
-  # rst_mask <- "E:\\WorldPop\\test_QGIS\\RWA_input\\mastergrid\\rwa_level1_100m_2015_2030.tif"
-  # rst_mask <- "E:\\WorldPop\\R_Script\\Covariates_Downloading\\data\\MLI_National_boundary.shp"
-
   if (!file.exists(output_dir)){
     stop(paste0("Directory does not exist [ ", basename(output_dir), " ]. "), call. = FALSE)
   }
@@ -72,31 +62,13 @@ g2c_download <- function(covariate=NULL,
   if (ISO %!in%  names(global_nb)){
     stop(paste0( "ISO ", ISO , "is not in the list of avalible covairets. Please use a corect ISO of the country"), call. = FALSE)
   }
-
-
+  
   if (!is.null(rst_mask) && !file.exists(rst_mask)){
     stop(paste0("File does not exist [ ", basename(rst_mask), " ]. "), call. = FALSE)
   }
 
-
-
   if (covariate %!in% get_names_covariates()) {
     stop(paste0(covariate, " does not exist. Please check avalible covariates by using function [ covariates_aval() ]"), call. = FALSE)
-  }
-
-  rst_mask_f_extention <- getExtension(rst_mask)
-
-  if (rst_mask_f_extention %!in% c("gpkg", "tif", "shp")){
-    stop(paste0(basename(rst_mask), " is ot vector or raster file. Only 'gpkg', 'tif' or 'shp' are accepted."), call. = FALSE)
-  }
-
-  if (!is.null(rst_mask) ){
-    if (rst_mask_f_extention == "tif"){
-      x_rst <- terra::rast(rst_mask)
-      stopifnot(terra::hasValues(x_rst))
-    }else{
-      x_rst <- terra::vect(rst_mask)
-    }
   }
 
   ### checking  arguments fo
@@ -126,9 +98,7 @@ g2c_download <- function(covariate=NULL,
     keep_tmp <- FALSE
   }
 
-
   if (is.null(rst_mask)){
-
     file_remote <- get_path_covariates(ISO, covariate, prj_year)
     dest_file <- file.path(output_dir, paste0(get_fn_without_ext(file_remote),".tif"))
 
@@ -141,7 +111,24 @@ g2c_download <- function(covariate=NULL,
                          quiet=quiet)
     }
 
-  }else{
+    return(invisible(TRUE))
+  }
+
+
+  rst_mask_f_extention <- getExtension(rst_mask)
+
+  if (rst_mask_f_extention %!in% c("gpkg", "tif", "shp")){
+    stop(paste0(basename(rst_mask), " is ot vector or raster file. Only 'gpkg', 'tif' or 'shp' are accepted."), call. = FALSE)
+  }
+
+  if (!is.null(rst_mask) ){
+    if (rst_mask_f_extention == "tif"){
+      x_rst <- terra::rast(rst_mask)
+      stopifnot(terra::hasValues(x_rst))
+    }else{
+      x_rst <- terra::vect(rst_mask)
+    }
+  }
 
     nb_countries <- global_nb[[ISO]]
     countries_download <- c(ISO, nb_countries)
@@ -246,7 +233,7 @@ g2c_download <- function(covariate=NULL,
     }
     if (file.exists(vrt_file_db)) invisible(file.remove(vrt_file_db))
 
-  }
+
 
 
   return(invisible(TRUE))
