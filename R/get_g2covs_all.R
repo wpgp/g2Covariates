d7c361b9f5b6038a91c27da4584a48c47bfc0836
bondarenkom <m.bondarenko@gmail.com>
2025-10-22 20:33:32 +0100

    Updated the following functions: g2c_download_all g2c_download_all_covs g2c_download_years

--- R/get_g2covs_all.R
+++ R/get_g2covs_all.R
@@ -1,8 +1,37 @@
-
-
-## One COVARIATE for ALL AVAILABLE YEARS
-
-# Define function
+## 
+## Function developed by Dr. Somnath Chaudhuri
+#' Function to download a geospatial covariate for multiple years
+#'
+#' @rdname g2c_download_years
+#' @param covariate A character, name of the covariate to download
+#' @param ISO A character, ISO code(s) of a specified country
+#' @param years Numerical, the years of the covariate
+#' @param output_dir Character vector containing the path to the directory for
+#'        writing output files. Default is the temp directory.
+#' @param rst_mask path to mask file (Default NULL)
+#' @param quiet If TRUE, suppress status messages (if any), and the progress bar.
+#' @param ...	Additional arguments:\cr
+#'        \code{ftp_srv}: Boolean, If TRUE then WorldPop ftp server will
+#'        be used otherwise http.\cr
+#'        \code{method}: Method to be used for downloading files. Current download
+#'        methods are "internal", "wininet" (Windows only) "libcurl",  "wget" and "curl",
+#'        and there is a value "auto"
+#'        \code{cells_touches}: 	logical. If TRUE, all cells touched by lines or ,
+#'        polygons are affected, not just those on the line render path
+#'         or whose center point is within the polygon.\cr
+#'        \code{keep_tmp}: 	logical. If TRUE, do not remove ,
+#'        any temporal files downloaded.\cr
+#' @return A character, path to downloaded file
+#' @export
+#' @examples
+#' \dontrun{
+#' g2c_download_years(covariate=NULL,
+#'                    ISO=NULL,
+#'                    years=c(2021,2024),
+#'                    rst_mask=NULL,
+#'                    output_dir=NULL,
+#'                    quiet=TRUE)
+#' }
 g2c_download_years <- function(covariate, ISO, years, output_dir, rst_mask = NULL, quiet = FALSE, ...) {
   for (yr in years) {
     message("Downloading ", covariate, " for year ", yr)
@@ -27,9 +56,41 @@ g2c_download_years <- function(covariate, ISO, years, output_dir, rst_mask = NUL
 
 #========================================================================
 
-## All COVARIATES for a ONE YEAR with USER OWN Shapefiles
+## 
+## Function developed by Dr. Somnath Chaudhuri
+#' Function to download all geospatial covariates for a year
+#'
+#' @rdname g2c_download_all_covs
+#' @param ISO A character, ISO code(s) of a specified country
+#' @param year Numerical, the year of the covariate
+#' @param output_dir Character vector containing the path to the directory for
+#'        writing output files. Default is the temp directory.
+#' @param rst_mask path to mask file (Default NULL)
+#' @param quiet If TRUE, suppress status messages (if any), and the progress bar.
+#' @param ...	Additional arguments:\cr
+#'        \code{ftp_srv}: Boolean, If TRUE then WorldPop ftp server will
+#'        be used otherwise http.\cr
+#'        \code{method}: Method to be used for downloading files. Current download
+#'        methods are "internal", "wininet" (Windows only) "libcurl",  "wget" and "curl",
+#'        and there is a value "auto"
+#'        \code{cells_touches}: 	logical. If TRUE, all cells touched by lines or ,
+#'        polygons are affected, not just those on the line render path
+#'         or whose center point is within the polygon.\cr
+#'        \code{keep_tmp}: 	logical. If TRUE, do not remove ,
+#'        any temporal files downloaded.\cr
+#' @return A character, path to downloaded file
+#' @export
+#' @examples
+#' \dontrun{
+#' g2c_download_all_covs(ISO=NULL,
+#'                       year=2021,
+#'                       rst_mask=NULL,
+#'                       output_dir=NULL,
+#'                       quiet=TRUE)
+#' }
 
-g2c_download_all_covs <- function(ISO, year, output_dir, rst_mask = NULL, quiet = FALSE) {
+
+g2c_download_all_covs <- function(ISO, year, output_dir, rst_mask = NULL, quiet = FALSE, ...) {
   
   all_covs <- get_names_covariates()
   
@@ -43,7 +104,8 @@ g2c_download_all_covs <- function(ISO, year, output_dir, rst_mask = NULL, quiet 
         ftp_srv = FALSE,       ## avoids FTP by default
         rst_mask = rst_mask,
         output_dir = output_dir,
-        quiet = quiet
+        quiet = quiet,
+        ...
       )
     }, error = function(e) {
       message("Failed: ", cov, " | ", e$message)
@@ -55,9 +117,38 @@ g2c_download_all_covs <- function(ISO, year, output_dir, rst_mask = NULL, quiet 
 
 
 
-#========================================================================
-
-## All COVARIATES for ALL AVAILABLE YEARS
+## 
+## Function developed by Dr. Somnath Chaudhuri
+#' Function to download all geospatial covariates for multiple year
+#'
+#' @rdname g2c_download_all
+#' @param ISO A character, ISO code(s) of a specified country
+#' @param years Numerical, the years of the covariate
+#' @param output_dir Character vector containing the path to the directory for
+#'        writing output files. Default is the temp directory.
+#' @param rst_mask path to mask file (Default NULL)
+#' @param quiet If TRUE, suppress status messages (if any), and the progress bar.
+#' @param ...	Additional arguments:\cr
+#'        \code{ftp_srv}: Boolean, If TRUE then WorldPop ftp server will
+#'        be used otherwise http.\cr
+#'        \code{method}: Method to be used for downloading files. Current download
+#'        methods are "internal", "wininet" (Windows only) "libcurl",  "wget" and "curl",
+#'        and there is a value "auto"
+#'        \code{cells_touches}: 	logical. If TRUE, all cells touched by lines or ,
+#'        polygons are affected, not just those on the line render path
+#'         or whose center point is within the polygon.\cr
+#'        \code{keep_tmp}: 	logical. If TRUE, do not remove ,
+#'        any temporal files downloaded.\cr
+#' @return A character, path to downloaded file
+#' @export
+#' @examples
+#' \dontrun{
+#' g2c_download_all(ISO=NULL,
+#'                  years=2021,
+#'                  rst_mask=NULL,
+#'                  output_dir=NULL,
+#'                  quiet=TRUE)
+#' }
 
 # Define Function
 g2c_download_all <- function(ISO, years, output_dir, rst_mask = NULL, quiet = FALSE, ...) {
